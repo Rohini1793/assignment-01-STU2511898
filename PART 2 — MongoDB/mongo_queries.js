760f3b4c4a7b125944a66fd65920bf1d6e438278
Rohini1793 <rohini.patil@bkbck.edu.in>
2026-03-16 21:27:28 +0530

    Update mongo_queries.js

--- PART 2 — MongoDB/mongo_queries.js
+++ PART 2 — MongoDB/mongo_queries.js
@@ -1,1 +1,17 @@
+// OP1 insertMany
+db.products.insertMany([...])
 
+// OP2
+db.products.find({category:"Electronics",price:{$gt:20000}})
+
+// OP3
+db.products.find({category:"Groceries",expiry_date:{$lt:"2025-01-01"}})
+
+// OP4
+db.products.updateOne(
+{product_name:"iPhone 15"},
+{$set:{discount_percent:10}}
+)
+
+// OP5
+db.products.createIndex({category:1})
